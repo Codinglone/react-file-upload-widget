03ac97c2aeb4b81ed457b7b4214f6af16790461b
Fabrice <codinglone@gmail.com>
2023-04-12 15:39:49 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Uploader } from "uploader";
+import { UploadButton } from "react-uploader";
+
+const uploader = Uploader({ apiKey: "public_12a1y8D7g6sCzJbMRTZZ4fKrw6gU" });
+function App() {
+  return (
+    <div>
+      <UploadButton
+        uploader={uploader}
+        options={{ multi: false }}
+        onComplete={(files) => alert(files.map((x) => x.fileUrl).join("\n"))}
+      >
+        {
+          ({onClick}) => <button onClick={onClick}>Upload a file...</button>
+        }
+      </UploadButton>
+    </div>
+  );
+}
+
+export default App;
